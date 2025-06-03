1a3fb9d070e402fc85fc09a6c47c5160a11c2e26
Ghabriel-Silva <ghaabriieel@gmail.com>
2025-06-03 18:20:03 -0300

    Adicionando metodo que lida com o envio

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-class validaFom{
-    constructor(){
+class validaFom {
+    constructor() {
         this.formulario = document.querySelector('.formulario')
         this.event()
     }
 
-    event(){
-        this.formulario.addEventListener('submit', e =>{
-          this.handleSend(e)
+    event() {
+        this.formulario.addEventListener('submit', e => {
+            this.handleToSend(e)
         })
     }
 
-    handleSend(e){
+    handleToSend(e) {
         e.preventDefault()
+        
     }
 
 }
+
+const validForm = new validaFom()
